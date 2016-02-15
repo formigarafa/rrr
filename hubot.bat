6667acd7e59b1f151ff3cbd24b5927a7ed152db6
Rafael Santos <formigarafa@gmail.com>
2016-02-15 15:14:32 +1300

    Update hubot.bat

--- hubot.bat
+++ hubot.bat
@@ -1,25 +1,27 @@
+# create container
+sudo docker run --rm -it node /bin/bash
 
+# setup environment
 npm install -g yo generator-hubot coffee-script
 apt-get -q update
 apt-get -qy install git-core redis-server
 service redis-server start
+
+# create user for robot
 adduser --disabled-password --gecos "" yeoman; echo "yeoman ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
-
-
 su - yeoman
 
-mkdir myhubot
-cd myhubot
+# creating fresh robot
+mkdir rrr
+cd rrr
 yo hubot
+npm install -S hubot-script-shellcmd
+cp -R node_modules/hubot-script-shellcmd/bash .
+# then add more scripts to bash/handlers
 
-git config --global user.email 'formigarafa@gmail.com'
-git config --global user.name 'Rafael Santos'
+# using a preexisting robot
+git clone https://github.com/formigarafa/rrr.git
+cd rrr
 
-git init
-git add .
-git commit -m 'init'
-
-./bin/slack
-
-HUBOT_SLACK_TOKEN=xoxb-21353567475-iip03ihwJib5CImPFSh3zdlL ./bin/hubot --adapter slack
+# starting the robot
+HUBOT_SHELLCMD_KEYWORD='bash' HUBOT_SLACK_TOKEN=xoxb-21353567475-iip03ihwJib5CImPFSh3zdlL ./bin/hubot --adapter slack
